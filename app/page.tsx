c6439070e48003e98792c06aedc85d09d4cb245c
Barkın Balcı <barkin.balci@gmail.com>
2023-12-25 22:27:46 +0300

    feat: updated the main page

--- app/page.tsx
+++ app/page.tsx
@@ -1,60 +1,77 @@
-import { Button } from "@/components/ui/button"
-import { Separator } from "@/components/ui/separator"
-import { Input } from "@/components/ui/input"
-
+import { Metadata } from "next"
+import Image from "next/image"
 import Link from "next/link"
 
-export default function Home() {
+import { Button } from "@/components/ui/button"
+import { UserAuthForm } from "./authForm"
+
+export const metadata: Metadata = {
+  title: "Authentication",
+  description: "Authentication forms built using the components.",
+}
+
+export default function AuthenticationPage() {
   return (
-    <main className="flex min-h-screen bg-black flex-col items-center justify-between">
-      <div className="min-h-screen text-white flex flex-col md:flex-row">
-        <div className="w-full md:w-3/5 bg-gray-700 p-12 space-y-6 flex flex-col justify-between hidden md:flex">
-          <div>
-            <h1 className="text-4xl font-bold">CoilCrypt</h1>
+      <div className="container relative h-[100vh] flex-col items-center justify-center grid lg:max-w-none lg:grid-cols-2 lg:px-0">
+        <Button className="absolute right-4 top-4 md:right-8 md:top-8" variant="ghost">Login</Button>
+        <div className="relative hidden h-full flex-col bg-muted p-10 text-white lg:flex dark:border-r">
+          <div className="absolute inset-0 bg-zinc-900" />
+          <div className="relative z-20 flex items-center text-lg font-medium">
+            <svg
+              xmlns="http://www.w3.org/2000/svg"
+              viewBox="0 0 24 24"
+              fill="none"
+              stroke="currentColor"
+              strokeWidth="2"
+              strokeLinecap="round"
+              strokeLinejoin="round"
+              className="mr-2 h-6 w-6"
+            >
+              <path d="M15 6v12a3 3 0 1 0 3-3H6a3 3 0 1 0 3 3V6a3 3 0 1 0-3 3h12a3 3 0 1 0-3-3" />
+            </svg>
+            CoilCrypt
           </div>
-          <div>
-            <p className="text-lg">
-            &ldquo;This library has saved me countless hours of work and helped me deliver stunning designs to my clients
-              faster than ever before.&rdquo;
-            </p>
-            <p className="mt-4 text-lg font-semibold">Sofia Davis</p>
+          <div className="relative z-20 mt-auto">
+            <blockquote className="space-y-2">
+              <p className="text-lg">
+                &ldquo;This library has saved me countless hours of work and
+                helped me deliver stunning designs to my clients faster than
+                ever before.&rdquo;
+              </p>
+              <footer className="text-sm">Sofia Davis</footer>
+            </blockquote>
           </div>
         </div>
-        <div className="w-full md:w-2/5 bg-black p-12 space-y-6">
-          <div className="flex justify-end">
-            <Button className="text-white" variant="ghost">
-              Login
-            </Button>
-          </div>
-          <div className="max-w-md mx-auto">
-            <h2 className="text-3xl font-bold">Create an account</h2>
-            <p className="mt-2">Enter your email below to create your account</p>
-            <div className="mt-6">
-              <Input placeholder="name@example.com" /> 
-              <Button className="mt-4 w-full">Sign in with Email</Button>
+        <div className="lg:p-8">
+          <div className="mx-auto flex w-full flex-col justify-center space-y-6 sm:w-[350px]">
+            <div className="flex flex-col space-y-2 text-center">
+              <h1 className="text-2xl font-semibold tracking-tight">
+                Create an account
+              </h1>
+              <p className="text-sm text-muted-foreground">
+                Enter your email below to create your account
+              </p>
             </div>
-            <div className="mt-6 flex flex-no-wrap items-center justify-between">
-              <hr className="flex-grow bg-gray-600" />
-              <p className="px-4 text-sm text-gray-400">OR CONTINUE WITH</p>
-              <hr className="flex-grow bg-gray-600" />
-            </div>
-            <div className="mt-4">
-              <Button className="w-full" variant="secondary">GitHub</Button>
-            </div>
-            <p className="mt-6 text-sm text-gray-400">
-              By clicking continue, you agree to our
-              <Link className="text-blue-500" href="#">
+            <UserAuthForm />
+            <p className="px-8 text-center text-sm text-muted-foreground">
+              By clicking continue, you agree to our{" "}
+              <Link
+                href="/terms"
+                className="underline underline-offset-4 hover:text-primary"
+              >
                 Terms of Service
               </Link>{" "}
               and{" "}
-              <Link className="text-blue-500" href="#">
+              <Link
+                href="/privacy"
+                className="underline underline-offset-4 hover:text-primary"
+              >
                 Privacy Policy
               </Link>
-              .{"\n                  "}
+              .
             </p>
           </div>
         </div>
       </div>
-    </main>
   )
 }
