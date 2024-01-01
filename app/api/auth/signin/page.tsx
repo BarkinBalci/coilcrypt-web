03ce6ff34acd1c1ba3f969d7c601893b0d6a5dcc
Barkın Balcı <barkin.balci@gmail.com>
2024-01-02 00:02:55 +0300

    Revert "feat: safeguard incase callback fails"
    This reverts commit ec79bdd3347f3b37ec07d1b056075757059e0d89.

--- app/api/auth/signin/page.tsx
+++ app/api/auth/signin/page.tsx
@@ -1,23 +1,14 @@
 "use client";
 import * as React from "react";
-import { useSession } from "next-auth/react";
-import { useRouter } from "next/router";
 
-import { UserAuthForm } from "@/components/ui/authForm";
+import { UserAuthForm } from "@/components/ui/authForm"
+
 
 export default function AuthenticationPage() {
-  const { data: session, status: loading } = useSession();
-  const router = useRouter();
-
-  React.useEffect(() => {
-    if (session) {
-      router.push("/vault");
-    }
-  }, [session]);
 
   return (
     <div style={{ height: '100vh', display: 'flex', flexDirection: 'column' }}>
       <UserAuthForm/>
     </div>
-  );
+  )
 }
