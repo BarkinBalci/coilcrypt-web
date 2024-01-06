0fe945d17af4fe890a2eed6317b161dd9e26df2d
Barkın Balcı <barkin.balci@gmail.com>
2024-01-06 21:13:18 +0300

    feat: delete note

--- components/ui/noteItem.tsx
+++ components/ui/noteItem.tsx
@@ -6,10 +6,27 @@ interface NoteItemProps {
   note: any;
 }
 
+async function deleteNote(noteId: String) {
+  const response = await fetch("/api/vault/deleteNote", {
+    method: "POST",
+    headers: {
+      "Content-Type": "application/json",
+    },
+    body: JSON.stringify({ noteId }),
+  });
+
+  if (!response.ok) {
+    throw new Error(`HTTP error! status: ${response.status}`);
+  }
+
+  const message = await response.json();
+  console.log(message);
+}
+
 export function NoteItem({note}: NoteItemProps) {
 
-    return(
-        <div
+    return (
+      <div
         key={note.id}
         id={note.id.toString()}
         className="collapse collapse-arrow bg-base-200"
@@ -29,10 +46,14 @@ export function NoteItem({note}: NoteItemProps) {
             <button className="btn btn-warning">
               <Icons.edit />
             </button>
-            <button className="btn btn-error">
+            <button
+              className="btn btn-error"
+              onClick={() => deleteNote(note.id.toString())}
+            >
               <Icons.trash />
             </button>
           </div>
         </div>
-      </div>)
+      </div>
+    );
 }
