e8832407dd50f72585dd5ecda3d8094ad8e866ee
Barkın Balcı <barkin.balci@gmail.com>
2024-01-09 00:25:26 +0300

    chore: seperated the PBKDF2 encrypt/decrypt

--- lib/crypto.ts
+++ lib/crypto.ts
@@ -1,27 +1,10 @@
 export const decryptFile = async (file: File, password: string) => {
     const fileData = await file.arrayBuffer();
-    const salt = fileData.slice(0, 16);
-    const iv = fileData.slice(16, 32);
+    const salt = new Uint8Array(fileData.slice(0, 16));
+    const iv = new Uint8Array(fileData.slice(16, 32));
     const actualEncryptedData = fileData.slice(32);
-    const passwordKey = await window.crypto.subtle.importKey(
-        "raw",
-        new TextEncoder().encode(password),
-        "PBKDF2",
-        false,
-        ["deriveKey"]
-    );
-    const aesKey = await window.crypto.subtle.deriveKey(
-        {
-            name: "PBKDF2",
-            salt: salt,
-            iterations: 1000000,
-            hash: "SHA-256",
-        },
-        passwordKey,
-        { name: "AES-CBC", length: 256 },
-        false,
-        ["encrypt", "decrypt"]
-    );
+
+    const aesKey = await PBKDF2(password, salt, 1000000, "SHA-256");
     const decryptedContent = await window.crypto.subtle.decrypt(
         {
             name: "AES-CBC",
@@ -38,25 +21,7 @@ export const encryptFile = async (file: File, password: string): Promise<ArrayBu
     const fileData = await file.arrayBuffer();
     const salt = window.crypto.getRandomValues(new Uint8Array(16));
     const iv = window.crypto.getRandomValues(new Uint8Array(16));
-    const passwordKey = await window.crypto.subtle.importKey(
-        "raw",
-        new TextEncoder().encode(password),
-        "PBKDF2",
-        false,
-        ["deriveKey"]
-    );
-    const aesKey = await window.crypto.subtle.deriveKey(
-        {
-            name: "PBKDF2",
-            salt: salt,
-            iterations: 1000000,
-            hash: "SHA-256",
-        },
-        passwordKey,
-        { name: "AES-CBC", length: 256 },
-        false,
-        ["encrypt", "decrypt"]
-    );
+    const aesKey = await PBKDF2(password, salt, 1000000, "SHA-256");
     const encryptedData = await window.crypto.subtle.encrypt(
         {
             name: "AES-CBC",
@@ -75,4 +40,33 @@ export const encryptFile = async (file: File, password: string): Promise<ArrayBu
         salt.byteLength + iv.byteLength
     );
     return encryptedContent.buffer;
+};
+
+//Password Based Key Derivation Function 2
+export const PBKDF2 = async (
+    password: string,
+    salt: Uint8Array,
+    iterations: number,
+    hash: string
+) => {
+    const passwordKey = await window.crypto.subtle.importKey(
+        "raw",
+        new TextEncoder().encode(password),
+        "PBKDF2",
+        false,
+        ["deriveKey"]
+    );
+    const aesKey = await window.crypto.subtle.deriveKey(
+        {
+            name: "PBKDF2",
+            salt: salt,
+            iterations: iterations,
+            hash: hash,
+        },
+        passwordKey,
+        { name: "AES-CBC", length: 256 },
+        false,
+        ["encrypt", "decrypt"]
+    );
+    return aesKey;
 };
