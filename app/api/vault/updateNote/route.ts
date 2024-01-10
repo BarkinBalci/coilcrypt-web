75118511adf4c634b18e6b90855db3f9833957c0
Barkın Balcı <barkin.balci@gmail.com>
2024-01-10 15:45:20 +0300

    feat: added update revision date

--- app/api/vault/updateNote/route.ts
+++ app/api/vault/updateNote/route.ts
@@ -44,6 +44,7 @@ export async function POST(request: Request, response: Response) {
         data: {
             name,
             content,
+            revisionDate: new Date().toISOString()
         }
     });
 
